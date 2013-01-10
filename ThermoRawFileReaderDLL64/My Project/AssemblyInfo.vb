d742a52ac1178058a45b52f7acc656ad17114ba3
Brian LaMarche <brian.lamarche@pnnl.gov>
2013-01-10 17:41:28 +0000

    Added 64-bit support to the DLL.  The directories build to the bin directory (both platforms) bin\platform\configuration (e.g. release or debug)  The 64-bit project references the 64-bit COM Thermo reader.  It uses the same code that the x86 version does so you should be able to write or change code in either source and it will update to both projects.  There was no easy way to make the COM references change when you change the project platform type.  That is why we created two projects.

---
+++ ThermoRawFileReaderDLL64/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ThermoRawFileReaderDLL")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("PNNL")> 
+<Assembly: AssemblyProduct("ThermoRawFileReaderDLL")> 
+<Assembly: AssemblyCopyright("")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8ce85e93-c2b1-40b7-aced-9278fefc489f")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
