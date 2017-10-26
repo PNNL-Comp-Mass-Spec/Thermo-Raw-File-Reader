43f2875ba8451363419c683812903448e0ad209d
alchemistmatt <matthew.monroe@pnnl.gov>
2017-10-25 19:36:58 -0700

    Add FindScanFilters batch file and results

---
+++ UnitTests/Docs/FindScanFilters.bat
@@ -0,0 +1,6 @@
+rem The following command will examine all _ScanStatsEx.txt files in this directory 
+rem to summarize the scan filters present in the ScanStats files
+
+..\..\Test_ThermoRawFileReader\bin\Debug\Test_ThermoRawFileReader /GetFilters
+
+echo Done processing; see file ScanFiltersFound.txt
