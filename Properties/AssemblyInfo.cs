0fcfa6d2afd569f2c1c611fd9461999bfeea6643
alchemistmatt <matthew.monroe@pnnl.gov>
2017-11-02 17:47:48 -0700

    Revert "Remove Assemblyinfo file"
    This reverts commit 13a83981a8cbd7dd4609ebf2c9dc839cd01884a9.
    
    # Conflicts:
    #	ThermoRawFileReader.csproj

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ThermoRawFileReader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PNNL")]
+[assembly: AssemblyProduct("ThermoRawFileReader")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Assembly marked as NOT compliant. (MSFileReaderLib has an enum with non-compliant names,
+// and since it is an "Embedded Interop Type", the compiler complains about it if we mark
+// the whole assembly as CLS Compliant. Working around that problem by marking the assembly
+// as not CLS compliant, and marking all objects as CLS Compliant=true)
+[assembly: CLSCompliant(false)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f05d15cc-9842-4945-b011-004b09f0a2eb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2.0.*")]
